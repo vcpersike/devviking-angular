0cedc414411d8cb1c5bfbd3fa5b017db46754be9
vcpersike <vcpersike@gmail.com>
2024-11-25 02:45:52 -0300

    ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+];
